413af534ca87259bb8c29fa5e4d87e62a61be604
xblacks17 <mwale830@gmail.com>
2025-05-16 10:24:24 +0200

    new things

---
+++ android/build.gradle.kts
@@ -0,0 +1,25 @@
+// Root-level build file where you can add configuration options common to all sub-projects/modules.
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// This moves the build output to a central location outside each module
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
